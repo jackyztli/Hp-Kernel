9ce5c2684b2e5a813c1bffeb156797fbe5f225f4
jackyzt <jacky.szu.edu.cn@gmail.com>
2021-03-23 00:29:11 +0800

    lib/print.S kernel/main.c

---
+++ kernel/main.c
@@ -0,0 +1,18 @@
+/*
+ *  kernel/main.c
+ *
+ *  (C) 2021  Jacky
+ */
+
+#include "lib/print.h"
+
+int main()
+{
+     put_str("I'm a Kernel\n");
+	
+	while (1) {
+	
+	}
+	
+    return 0;
+}
---
+++ lib/print.h
@@ -0,0 +1,11 @@
+/*
+ *  lib/print.h
+ *
+ *  (C) 2021  Jacky
+ */
+#ifndef __PRINT_H
+#define __PRINT_H
+
+void put_str(const char *str);
+
+#endif
